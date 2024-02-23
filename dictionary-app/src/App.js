42908c9ad09422a21af9848f9a5f881ee44aa101
Mudipa kishan imayanga <128371632+mk1shan@users.noreply.github.com>
2024-02-23 22:26:58 +0530

    second

--- dictionary-app/src/App.js
+++ dictionary-app/src/App.js
@@ -1,86 +1,48 @@
-import { React, useState } from "react";
-import Axios from "axios";
+import axios from "axios";
+import { useState } from "react";
+import ListDetails from "./components/ListDetails";
 import "./App.css";
-import { FaSearch } from "react-icons/fa";
-import { FcSpeaker } from "react-icons/fc";
 
-function App() {
-// Setting up the initial states using react hook 'useState'
+//get css from description
+//install axios
 
-const [data, setData] = useState("");
-const [searchWord, setSearchWord] = useState("");
+export default function App() {
+  const [keyWord, setKeyWord] = useState("");
 
-// Function to fetch information on button 
-// click, and set the data accordingly
-function getMeaning() {
-	Axios.get(
-	`https://api.dictionaryapi.dev/api/v2/entries/en_US/${searchWord}`
-	).then((response) => {
-	setData(response.data[0]);
-	});
+  const [result, setResult] = useState(null);
+
+  const api = "https://api.dictionaryapi.dev/api/v2/entries/en";
+
+  async function handleSearch() {
+    try {
+      const res = await axios.get(`${api}/${keyWord}`);
+      console.log(res, "res");
+      setResult(res.data[0]);
+    } catch (e) {
+      console.log({ e });
+    }
+  }
+
+  function handleClear() {
+    setKeyWord("");
+    setResult(null);
+  }
+  return (
+    <div className="App">
+      <input value={keyWord} onChange={(e) => setKeyWord(e.target.value)} />
+      <button className="button" type="submit" onClick={handleSearch}>
+        Search
+      </button>
+      <button
+        disabled={!result}
+        className="button"
+        type="submit"
+        onClick={handleClear}
+      >
+        Clear
+      </button>
+
+      {result && <ListDetails {...{ result }} />}
+    </div>
+  );
 }
-
-// Function to play and listen the 
-// phonetics of the searched word
-function playAudio() {
-	let audio = new Audio(data.phonetics[0].audio);
-	audio.play();
-}
-
-return (
-	<div className="App">
-	<h1> Dictionary app</h1>
-	<div className="searchBox">
-
-	Taking user input
-		<input
-		type="text"
-		placeholder="Search..."
-		onChange={(e) => {
-			setSearchWord(e.target.value);
-		}}
-		/>
-		<button
-		onClick={() => {
-			getMeaning();
-		}}
-		>
-		<FaSearch size="20px" />
-		</button>
-	</div>
-	{data && (
-		<div className="showResults">
-		<h2>
-			{data.word}{" "}
-			<button
-			onClick={() => {
-				playAudio();
-			}}
-			>
-			<FcSpeaker size="26px" />
-			</button>
-		</h2>
-		<h4>Parts of speech:</h4>
-
-		
-<p>{data.meanings[0].partOfSpeech}</p>
-
-
-		<h4>Definition:</h4>
-
-		
-<p>{data.meanings[0].definitions[0].definition}</p>
-
-
-		<h4>Example:</h4>
-
-		
-<p>{data.meanings[0].definitions[0].example}</p>
-
-		</div>
-	)}
-	</div>
-);
-}
-
-export default App;
